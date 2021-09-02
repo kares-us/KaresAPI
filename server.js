a99b0d30f17f63c9032e2a383a84c3fa41426625
Zach Shearer <shearertzach@gmail.com>
2021-09-01 20:57:07 -0500

    .

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://kares.us");
+  res.header("Access-Control-Allow-Origin", "https://www.kares.us");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
   next();
